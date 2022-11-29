5d8afc71cfba55cafc6d80ffcba6584c27fa541a
Micha <111045381+Micha-JS@users.noreply.github.com>
2022-11-29 15:29:01 +0100

    update internet_app

--- .ipynb_checkpoints/internet_world-checkpoint.py
+++ .ipynb_checkpoints/internet_world-checkpoint.py
@@ -1,3 +1,6 @@
+@st.cache
+
+
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
@@ -9,6 +12,9 @@ def load_data(path):
     df = pd.read_csv(path)
     return df
 
+st.title("Internet usage per country in percentage")
+st.header("Data Exploration")
+
 
 with open('/Users/mjs/Documents/DS_Projects/My_first_streamlitapp/countries.geojson') as f:
     countries = geojson.load(f)
@@ -18,6 +24,13 @@ internet_df = pd.read_csv('/Users/mjs/Documents/DS_Projects/My_first_streamlitap
 internet_df.rename(columns={'Individuals using the Internet (% of population)':'usage_internet'}, inplace=True)
 
 
+
+
+if st.checkbox("Show Dataframe"):
+    st.subheader("This is my dataset:")
+    st.dataframe(data=internet_df)
+
+
 fig = px.choropleth(internet_df[internet_df['Year'] == 2010], geojson=countries, locations='Code', color='usage_internet',
                            color_continuous_scale="Viridis",
                            scope='world',
@@ -25,4 +38,4 @@ fig = px.choropleth(internet_df[internet_df['Year'] == 2010], geojson=countries,
                            labels={'usage_internet':'Individuals using the Internet in %'}
                           )
 
-fig.show()
+st.plotly_chart(fig)
