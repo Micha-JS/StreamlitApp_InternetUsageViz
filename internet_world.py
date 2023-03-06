c5f0cd3e26f9298a0ce31464b7589457a0fc967c
Micha <111045381+Micha-JS@users.noreply.github.com>
2023-03-06 18:17:02 +0100

    update data

--- internet_world.py
+++ internet_world.py
@@ -6,32 +6,22 @@ import plotly.io as pio
 import geojson
 import pandas as pd
 
+# Load the data
 
-def load_data(path):
-    df = pd.read_csv(path)
-    return df
+with open('./data/countries.geojson') as f:
+    countries = geojson.load(f)
 
-def load_geojson(path):
-    gj = geojson.load(path)
-    return gj
+internet_df = pd.read_csv('data/share-of-individuals-using-the-internet.csv')
+internet_df.rename(columns={'Individuals using the Internet (% of population)':'internet_usage'}, inplace=True)
 
-st.title("Internet usage per country in percentage")
+st.title("Internet usage per country in percent")
 st.header("Data Exploration")
 
 
-with open('countries.geojson') as f:
-    countries = geojson.load(f)
 
 
-internet_df = pd.read_csv('share-of-individuals-using-the-internet.csv')
-internet_df.rename(columns={'Individuals using the Internet (% of population)':'usage_internet'}, inplace=True)
-
-#countries = load_geojson('/Users/mjs/Documents/DS_Projects/My_first_streamlitapp/countries.geojson')
-
-
-
-if st.checkbox("Show Dataframe"):
-    st.subheader("This is my dataset:")
+if st.checkbox("Show data"):
+    st.subheader("Table with internet usage in percent per country:")
     st.dataframe(data=internet_df)
 
 # Setting up columns
@@ -39,7 +29,7 @@ left_column, middle_column, right_column = st.columns([3, 1, 1])
 
 # Widgets: selectbox years
 years = sorted(pd.unique(internet_df['Year']))
-year = left_column.selectbox("Choose a Year", years)
+year = left_column.selectbox("Choose a year", years)
 
 # Flow control and plotting
 
@@ -47,22 +37,25 @@ year = left_column.selectbox("Choose a Year", years)
 
 fig = px.choropleth(internet_df[internet_df['Year'] == year], 
                     geojson=countries, locations='Code', 
-                    color='usage_internet',
+                    color='internet_usage',
                     color_continuous_scale="thermal",
                     scope='world',
                     featureidkey="properties.ISO_A3",
-                    labels={'usage_internet':'Individuals using the Internet in %'},
+                    labels={'internet_usage':'Individuals using the internet in %'},
                     width=800, 
                     height=400
                           )
 
 
 
-st.plotly_chart(fig, width=800, height=400)
+st.plotly_chart(fig, width=1200, height=600)
 
 
 
-fig_1 = px.line(internet_df[internet_df['Code'] == 'SAU'], x="Year", y="usage_internet", title='Population internet usage')
+fig_1 = px.line(internet_df[internet_df['Code'] == 'SAU'],
+                x="Year",
+                y="internet_usage",
+                title='Population internet usage')
 
 
 st.plotly_chart(fig_1)
